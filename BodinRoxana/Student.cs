31f191f38ccdc14f24d1362d12878ae03eef77d8
Alex Andricsak <alex.andricsak@gmail.com>
2019-09-30 09:45:41 +0300

    lab2

---
+++ BodinRoxana/Student.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace BodinRoxana
+{
+    public class Student
+    {
+        public int Id;
+        public int An;
+        public String Nume;
+        public String Facultate;
+
+
+        public int getId()
+        {
+            return this.Id;
+        }
+
+        public void setId(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public int getAn()
+        {
+            return this.An;
+        }
+
+        public void setAn(int An)
+        {
+            this.An = An;
+        }
+
+        public String getNume()
+        {
+            return this.Nume;
+        }
+
+        public void setNume(String Nume)
+        {
+            this.Nume = Nume;
+        }
+
+        public String getFacultate()
+        {
+            return this.Facultate;
+        }
+
+        public void setFacultate(String Facultate)
+        {
+            this.Facultate = Facultate;
+        }
+        public Student(int id,String nume,int an,String facultate)
+        {
+            this.Id=id;
+            this.Nume=nume;
+            this.An=an;
+            this.Facultate=facultate;
+        }
+
+    }
+}
